e7b4d2fec59fdbbc36a5af05d0d2e7b63d9eb738
nvs123456 <nvs882004@gmail.com>
2025-05-12 17:22:56 +0700

    b2 commit 1

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
-
+    public static String func() {
+        System.out.println("b2 commit 1");
+        return "";
+    }
     public static void main(String[] args) {
         System.out.println("b1 commit 1");
         System.out.println("b1 commit 2");
