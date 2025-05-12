035c13582521a81208fa2051d99f8841fc24c578
nvs123456 <nvs882004@gmail.com>
2025-05-12 17:47:09 +0700

    b3 cm 1

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("b1 commit 1");
-        System.out.println("b1 commit 2");
+        System.out.println("Hello, World! in b3");
 
     }
 }
